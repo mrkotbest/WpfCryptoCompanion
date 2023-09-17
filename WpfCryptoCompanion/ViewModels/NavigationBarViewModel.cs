309b2292e6e3ea2dec5936be4aa0fc39c24e1459
mrkotbest <mrkotbest@gmail.com>
2023-09-17 22:11:43 +0300

    Change theme. Light / Dark.

--- WpfCryptoCompanion/ViewModels/NavigationBarViewModel.cs
+++ WpfCryptoCompanion/ViewModels/NavigationBarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows.Input;
 using WpfCryptoCompanion.Commands;
 using WpfCryptoCompanion.Services;
 
@@ -6,11 +7,27 @@ namespace WpfCryptoCompanion.ViewModels
 {
 	public class NavigationBarViewModel : BaseViewModel
 	{
+		private static bool _isDark;
+
 		public ICommand NavigateHomeCommand { get; }
+		public ICommand ChangeThemeCommand { get; }
 
         public NavigationBarViewModel(NavigationService<HomeViewModel> homeNaviService)
         {
             NavigateHomeCommand = new NavigateCommand<HomeViewModel>(homeNaviService);
+			ChangeThemeCommand = new ChangeThemeCommand();
         }
+
+        public static void ChangeTheme()
+        {
+			_isDark = !_isDark;
+			PaletteHelper paletteHelper = new();
+
+			ITheme theme = paletteHelper.GetTheme();
+			IBaseTheme baseTheme = _isDark ? new MaterialDesignDarkTheme() : new MaterialDesignLightTheme();
+
+			theme.SetBaseTheme(baseTheme);
+			paletteHelper.SetTheme(theme);
+		}
     }
 }
