b8bdd198b42ddd9d18a1983d79fe21c15ef18ec4
mrkotbest <mrkotbest@gmail.com>
2023-09-17 17:30:41 +0300

    Added Navigation Services with/without parameters.

---
+++ WpfCryptoCompanion/Services/ParamNavigationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WpfCryptoCompanion.Stores;
+using WpfCryptoCompanion.ViewModels;
+
+namespace WpfCryptoCompanion.Services
+{
+	public class ParamNavigationService<TParameter, TViewModel>
+        where TViewModel : BaseViewModel
+	{
+		private readonly NavigationStore _naviStore;
+		private readonly Func<TParameter, TViewModel> _createViewModel;
+
+        public ParamNavigationService(NavigationStore navigationStore, Func<TParameter, TViewModel> createViewModel)
+        {
+            _naviStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public void Navigate(TParameter parameter)
+        {
+            _naviStore.CurrentViewModel = _createViewModel(parameter);
+        }
+    }
+}
