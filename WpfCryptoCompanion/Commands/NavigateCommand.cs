ac25bd0a6cfcf13211a7624cd2aa1f1743b30fb8
mrkotbest <mrkotbest@gmail.com>
2023-09-17 17:44:06 +0300

    Added NavigateCommand for switching between Views by using only one command.

---
+++ WpfCryptoCompanion/Commands/NavigateCommand.cs
@@ -0,0 +1,21 @@
+﻿using WpfCryptoCompanion.Services;
+using WpfCryptoCompanion.ViewModels;
+
+namespace WpfCryptoCompanion.Commands
+{
+	public class NavigateCommand<TViewModel> : BaseCommand
+		where TViewModel : BaseViewModel
+	{
+		private readonly NavigationService<TViewModel> _naviService;
+
+        public NavigateCommand(NavigationService<TViewModel> navigationService)
+        {
+            _naviService = navigationService;
+        }
+
+        public override void Execute(object? parameter)
+		{
+			_naviService.Navigate();
+		}
+	}
+}
