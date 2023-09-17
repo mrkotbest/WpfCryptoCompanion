bd1cb15c66dee5708b1fa2b232c03269c439634e
mrkotbest <mrkotbest@gmail.com>
2023-09-17 17:23:56 +0300

    Created HomeViewModel & HomeView.

--- WpfCryptoCompanion/ViewModels/HomeViewModel.cs
+++ WpfCryptoCompanion/ViewModels/HomeViewModel.cs
@@ -1,21 +1,81 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Data;
 using WpfCryptoCompanion.Models;
+using WpfCryptoCompanion.Services;
 
 namespace WpfCryptoCompanion.ViewModels
 {
 	public class HomeViewModel : BaseViewModel
 	{
+		private readonly ApiHandler _apiHandler = new();
 		private readonly ObservableCollection<Coin> _coins = new();
+
+		private Coin _selectedCoin;
+		private string _searchText = string.Empty;
 
 		public IEnumerable<Coin> Coins => _coins;
 		public ICollectionView CoinsView => CollectionViewSource.GetDefaultView(Coins);
 
-        public HomeViewModel()
+		public bool CanShowDetails => SelectedCoin != null;
+		public string CoinId { get; set; } = "Currency";
+		public Coin SelectedCoin
+		{
+			get => _selectedCoin;
+			set
+			{
+				if (value == null) return;
+
+				_selectedCoin = value;
+				CoinId = SelectedCoin.Id;
+				OnPropertyChanged(nameof(SelectedCoin));
+				OnPropertyChanged(nameof(CoinId));
+				OnPropertyChanged(nameof(CanShowDetails));
+			}
+		}
+		public string SearchText
+		{
+			get => _searchText;
+			set
+			{
+				_searchText = value;
+				CoinsView.Refresh();
+				OnPropertyChanged(nameof(SearchText));
+			}
+		}
+
+		public HomeViewModel()
         {
-			_coins.Add(new Coin() { Id = "first", Rank = 1, Name = "BITCOIN", Price = 143.5f });
+			InitCoins();
+			CoinsView.Filter = FilterCoins;
         }
-    }
+
+		private bool FilterCoins(object? obj)
+		{
+			if (obj is Coin coin)
+			{
+				return coin.Name.Contains(SearchText, StringComparison.OrdinalIgnoreCase) ||
+					coin.Symbol.Contains(SearchText, StringComparison.OrdinalIgnoreCase);
+			}
+			return false;
+		}
+
+		private async void InitCoins()
+		{
+			await LoadCoinsAsync();
+		}
+
+		private async Task LoadCoinsAsync()
+		{
+			_coins.Clear();
+			var coins = await _apiHandler.GetCoinsAsync();
+			foreach (Coin? coin in coins)
+			{
+				_coins.Add(coin);
+			}
+		}
+	}
 }
